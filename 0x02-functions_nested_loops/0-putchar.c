1c022cb390b1f36ae5b0bdc91494f671dd6bfb52
luyosamson <luyosamson@gmail.com>
2022-06-07 16:13:17 -0700

    Correcting error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar as a message.
+ * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char i[] = "_putchar";
-	int j;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	for (j = 0; j < 9; j++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(i[j]);
+		_putchar(str[count]);
 	}
 	_putchar('\n');
 	return (0);
