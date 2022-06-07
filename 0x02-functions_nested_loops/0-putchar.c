f6bc7f90e039bbdaa5e028229ef2cf1566809888
Luyo Samson <64987320+luyosamson@users.noreply.github.com>
2022-06-08 00:50:27 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - Prints Holberton as a message.
+ * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int str[] = "_putchar";
-        int count, sz;
+        char i[] = "_putchar";
+	int j;
 
-        sz = sizeof(str) / sizeof(int);
-        for (count = 0; count < sz; count++)
-        {
-                putchar(str[count]);
-        }
-         putchar('\n');
-i        return (0);
+	for (j = 0; j < 9; j++)
+	{
+		_putchar(i[j]);
+	}
+	_putchar('\n');
+	return (0);
 }
 
